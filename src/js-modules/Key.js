e4b54f6ef2303bd0039c30a23a78c714121f4c13
Dzmitry Hlushak <hlushak.dzmitry@gmail.com>
2020-10-27 18:43:05 +0300

    feat: add class to create keyboard button

---
+++ src/js-modules/Key.js
@@ -0,0 +1,25 @@
+import createNode from './create-node.js';
+
+
+export default class Key {
+  constructor({small,shift,code}){
+    this.small = small;
+    this.shift = shift;
+    this.code = code;
+
+    this.isFnKey = Boolean(small.match(/Tab|Caps|Shift|Ctrl|Win|Alt|Enter|Back|Del|arr/gi));
+
+    if(shift && shift.match(/[^a-zA-Zа-яА-ЯёЁ0-9]/gi)) {
+      this.upperSymb = createNode('div', 'upper-symb', this.shift);
+    } else {
+      this.upperSymb = createNode('div', 'upper-symb', '');
+    }
+
+
+    this.letter = createNode('div', 'letter', small);
+    this.btn =  createNode('div', 'keyboard__btn', [this.upperSymb, this.letter], null,
+      ['code', this.code],
+      this.isFnKey ? ['fn', 'true'] : ['fn', 'false']
+    )
+  }
+};
