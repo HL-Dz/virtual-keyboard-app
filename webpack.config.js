593b9917fe293a4c4d84e74da0329c9e3c9d6527
Dzmitry Hlushak <hlushak.dzmitry@gmail.com>
2020-11-16 20:48:50 +0300

    feat: add support for arrow functions

--- webpack.config.js
+++ webpack.config.js
@@ -92,6 +92,11 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env"],
+            plugins: [
+              [
+                "@babel/plugin-proposal-class-properties"
+              ]
+            ]
           },
         },
       },
